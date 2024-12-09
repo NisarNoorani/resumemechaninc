3f46bca04fcde61c65a607df2600d9b91f6337fc
Nisar Noorani <160980997+NisarNoorani@users.noreply.github.com>
2024-12-09 14:51:52 +0500

    Bugs fixing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ function HomePage() {
   return (
     <div>
       <Head>
-        <title>E-Resume: Home</title>
+        <title>Resume Machanic: Home</title>
         <meta name="description" content="Single Page Resume Builder" />
         <link rel="icon" type="image/png" href="/icons/resume-icon.png" />
       </Head>
